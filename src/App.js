1230c4255e52eb564d4a3c2ca5404c10b69c1483
Sir Ronney Otieno <118272387+0tieno@users.noreply.github.com>
2024-02-13 19:44:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'Techies';
-  return <div class = "greeting">Hello {value} 😍</div>;
+  return <div className = "greeting">Hello {value} 😍</div>;
 }
 
 export default App;
